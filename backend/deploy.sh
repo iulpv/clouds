bf7ce3cb8194f3d72876ef05100d6b0d7f5663e4
iulpv <yul_karpova@mail.ru>
2023-01-10 14:11:35 +0500

    сделала деплой бэка

---
+++ backend/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+version=$(cat version.txt | awk -F. -v OFS=. '{$NF += 1 ; print}')
+echo $version > version.txt
+
+docker buildx build --platform linux/amd64 -f ./Dockerfile -t ${JOKE_API_REPOSITORY_NAME}:${version} .
+docker push ${JOKE_API_REPOSITORY_NAME}:${version}
+
+yc sls container revisions deploy \
+  --folder-id ${FOLDER_ID} \
+  --container-id ${JOKE_API_CONTAINER_ID} \
+  --memory 512M \
+  --cores 1 \
+  --execution-timeout 5s \
+  --concurrency 4 \
+  --environment AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID},AWS_PRIVATE_KEY=${AWS_PRIVATE_KEY},DOCUMENT_API_ENDPOINT=${DOCUMENT_API_ENDPOINT} \
+  --service-account-id ${JOKE_SERVICE_ACCOUNT_ID} \
+  --image ${JOKE_API_REPOSITORY_NAME}:${version}
