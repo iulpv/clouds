53deed556de3449734376822ccefe6144b00f06e
iulpv <yul_karpova@mail.ru>
2023-01-11 14:44:25 +0500

    добавила проверку на непустую шутку

--- frontend/project/src/hooks/post-joke.ts
+++ frontend/project/src/hooks/post-joke.ts
@@ -4,7 +4,7 @@ import {UserJoke} from "../components/add-joke";
 
 export function usePostJoke(joke : UserJoke, send : boolean) {
     useEffect(() => {
-        if (send) {
+        if (send && joke.body.length > 0 && joke.title.length > 0) {
             sendRequest(`https://d5dntuvm58qq5d8kvmk1.apigw.yandexcloud.net/joke?title=${joke.title}&body=${joke.body}`, 'POST')
                 .then(res => res.json())
                 .catch(error => {
